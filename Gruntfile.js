1190983d39837c3c414244cc0e82d2b406fa17f2
shutong037 <454194645@qq.com>
2017-10-31 09:19:08 +0800

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+/**
+ * Created by hejian on 2017/10/12.
+ */
+/*整个文件夹压缩*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        // css压缩
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,       //展开下面*通配符匹配的文件
+                    cwd: 'F:/hejian/模板/dome/1/js/2/',     //源文件根目录
+                    src: ['*.css', '!**/*.min.css'],  //不要做 “!” 指定的文件
+                    dest: 'F:/hejian/模板/dome/1/js/2/',        //压缩文件的输出目录
+                    ext: '.min.css'    //压缩文件的后缀名
+                }]
+            }
+        },
+        //js压缩
+        uglify: {
+            target: {
+                files: [{
+                    expand: true,       //展开下面*通配符匹配的文件
+                    cwd: 'F:/hejian/模板/dome/1/js/1/',     //源文件根目录
+                    src: ['*.js', '!**/*.min.js'],  //不要做 “!” 指定的文件
+                    dest: 'F:/hejian/模板/dome/1/js/1/',        //压缩文件的输出目录
+                    ext: '.min.js'    //压缩文件的后缀名
+                }]
+            }
+        }/*,
+        //css语法检测
+        csslint: {
+            options:{
+                csslintrc:'.csslint'
+            },
+            build:['E:/code/发票系统V0192/差客发票管理系统/Slwy.InvoiceManageSys(20170925_0192)/Slwy.InvoiceManageSys.Web/Content/css*.css']
+        },
+        //js语法检测
+        jshint:{
+            options:{
+                jshintrc:'.jshint'
+            },
+            build:['Gruntfile.js','E:/code/发票系统V0192/差客发票管理系统/Slwy.InvoiceManageSys(20170925_0192)/Slwy.InvoiceManageSys.Web/Content/js','!F:/hejian/模板/dome/1/js/1/!*.min.js'] //不检查压缩的js
+        }*/
+        /*  ,
+        //watch自动化
+      watch:{
+            build:{
+                files:['F:/hejian/模板/dome/1/js/1/!*.js','F:/hejian/模板/dome/1/js/2/!*.css'],
+                tasks:['cssmin','uglify'],
+                options:{spawn:false}
+            }
+        }*/
+
+    });
+
+
+
+
+
+    //告诉grunt我们将使用插件
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    //告诉grunt当我们在终端输入grunt时需要做些什么
+    grunt.registerInitTask('default',['cssmin','uglify']);
+};
---
+++ Gruntfile1.js
@@ -0,0 +1,27 @@
+/**
+ * Created by hejian on 2017/10/12.
+ */
+/*  单个文件压缩*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        cssmin: {
+            build: {
+                src:'F:/hejian/模板/dome/1/js/2/test1.css',
+                dest:'F:/hejian/模板/dome/1/js/2/test1.min.css'
+            }
+        },
+        uglify: {
+            build: {
+                src: 'F:/hejian/模板/dome/1/js/1/test1.js',
+                dest: 'F:/hejian/模板/dome/1/js/1/test1.min.js'
+            }
+        }
+    });
+
+    //告诉grunt我们将使用插件
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    //告诉grunt当我们在终端输入grunt时需要做些什么
+    grunt.registerInitTask('default',['cssmin','uglify']);
+};
